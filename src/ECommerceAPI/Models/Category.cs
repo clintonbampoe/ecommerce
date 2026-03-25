c25065b9af40d8ef3a50af270cdffc94bbc80560
clint@debian <clintbamp@gmail.com>
2026-03-25 23:28:20 +0000

    feat: initialized all Collections in affected classes to empty lists instead of null to make them easier to work with

--- src/ECommerceAPI/Models/Category.cs
+++ src/ECommerceAPI/Models/Category.cs
@@ -6,5 +6,5 @@ public class Category : ICategory
 {
     public int Id { get; set; }
     [MaxLength(32)] public string Name { get; set; } = string.Empty;
-    public ICollection<IProduct> Products { get; set; }
+    public ICollection<IProduct> Products { get; set; } = [];
 }
