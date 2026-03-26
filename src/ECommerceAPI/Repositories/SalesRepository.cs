aa6f840fb86165a26c865655d01df9a697cb52f8
clint@debian <clintbamp@gmail.com>
2026-03-26 21:26:20 +0000

    chore: fixed null reference warnings by checking if the navigation property is not null and providing a default property otherwise

--- src/ECommerceAPI/Repositories/SalesRepository.cs
+++ src/ECommerceAPI/Repositories/SalesRepository.cs
@@ -22,36 +22,40 @@ public class SalesRepository
 
     public async Task<IEnumerable<ProductSaleDto>?> GetProductSalesByProductId(int productId)
     {
-        // TODO: handle null reference in the services layer
         var productSales = await _dbContext.ProductSales
             .Where(ps => ps.ProductId == productId)
             .Select(ps => new ProductSaleDto()
             {
                 ProductId = ps.ProductId,
                 SalesId = ps.SalesId,
-                ProductName = ps.Product.Name,
+                ProductName = ps.Product != null ? ps.Product.Name : "Name Not Found",
                 TotalCost = ps.TotalCost
             }).ToListAsync();
 
         return productSales;
     }
 
-    public async Task<IEnumerable<ProductSaleDto?>> GetProductSalesbySalesId(int salesId)
+    public async Task<IEnumerable<ProductSaleDto?>> GetProductSalesBySalesId(int salesId)
     {
-        // TODO: handle null reference in services layer
         var productSales = await _dbContext.ProductSales
             .Where(ps => ps.SalesId == salesId)
             .Select(ps => new ProductSaleDto()
             {
                 ProductId = ps.ProductId,
                 SalesId = ps.SalesId,
-                SaleDate = ps.Sale.SaleDate,
+                SaleDate = ps.Sale != null ? ps.Sale.SaleDate : DateTime.Now,
                 TotalCost = ps.TotalCost
             }).ToListAsync();
 
         return productSales;
     }
 
+    /*
+     * The ProductSale table is accessed from the SalesRepository.
+     * This is because every time a Sale object is created in the db, a ProductSale object is created immediately.
+     * Hence, ProductSale is mainly dependent on Sale so it did not make sense to make its own repo and service.
+     * Also, the client should only interact with the Sale service
+     */
     public async Task<IEnumerable<ProductSale>> CreateProductSale(SalesCreateDto dto)
     {
         var newSale = new Sale()
