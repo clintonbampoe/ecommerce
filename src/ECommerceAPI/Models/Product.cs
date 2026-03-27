06801c76f73c8c12119c46e9d60f40d465914a59
clint@debian <clintbamp@gmail.com>
2026-03-27 22:17:46 +0000

    feat: added dto conversion to model as constructor for centralized conversion

--- src/ECommerceAPI/Models/Product.cs
+++ src/ECommerceAPI/Models/Product.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using ECommerceAPI.Models.Dtos;
 
 namespace ECommerceAPI.Models;
 
@@ -6,7 +7,7 @@ public class Product
 {
     public int Id { get; set; }
     [MaxLength(64)] public string Name { get; set; } = string.Empty;
-    public double Price { get; set; }
+    public decimal Price { get; set; }
     public int QuantityInStock { get; set; }
     public DateTime DateAdded { get; set; }
     
@@ -14,4 +15,18 @@ public class Product
     public Category? Category { get; set; }
 
     public ICollection<ProductSale> ProductSales { get; set; } = [];
+
+    public Product()
+    {
+        
+    }
+
+    public Product(ProductDto dto)
+    {
+        Id = dto.Id;
+        Name = dto.Name;
+        Price = dto.Price;
+        QuantityInStock = dto.QuantityInStock;
+        CategoryId = dto.CategoryId;
+    }
 }
