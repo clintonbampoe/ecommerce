e25960c2be55ff2829d7a18029338cd6fb08d2a6
clint@debian <clintbamp@gmail.com>
2026-03-27 22:18:42 +0000

    feat: added ProductDto to make transferring data and objects from clients easier. This way client doesn't have to worry themselves with the navigation properties

---
+++ src/ECommerceAPI/Models/Dtos/ProductDto.cs
@@ -0,0 +1,24 @@
+namespace ECommerceAPI.Models.Dtos;
+
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int QuantityInStock { get; set; }
+    public int CategoryId { get; set; }
+    
+    public ProductDto()
+    {
+        
+    }
+
+    public ProductDto(Product product)
+    {
+        Id = product.Id;
+        Name = product.Name;
+        Price = product.Price;
+        QuantityInStock = product.QuantityInStock;
+        CategoryId = product.CategoryId;
+    }
+}
