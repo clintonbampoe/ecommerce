1ff055c458fba90ce9da168288f7fc2d77631b0c
clint@debian <clintbamp@gmail.com>
2026-03-28 23:19:26 +0000

    chore: updated db, minor fixes and improvements

---
+++ src/ECommerceAPI/Migrations/20260328230519_MinorDbReinforcements.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MinorDbReinforcements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OrderDate",
+                table: "Sales",
+                newName: "SaleDate");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalCost",
+                table: "ProductSales",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalCost",
+                table: "ProductSales");
+
+            migrationBuilder.RenameColumn(
+                name: "SaleDate",
+                table: "Sales",
+                newName: "OrderDate");
+        }
+    }
+}
