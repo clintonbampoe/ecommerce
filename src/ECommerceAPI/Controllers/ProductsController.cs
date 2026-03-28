6959bfae1c29aa846085d382fcbc4421e745317d
clint@debian <clintbamp@gmail.com>
2026-03-28 23:20:41 +0000

    feat: added controller for Product

---
+++ src/ECommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using ECommerceAPI.Models.Dtos;
+using ECommerceAPI.Services;
+
+namespace ECommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetProducts()
+        {
+            return Ok(await _productService.GetAllProducts());
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetProduct(int id)
+        {
+            return Ok(await _productService.GetProductById(id));
+        }
+        
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> PostProduct(ProductDto product)
+        {
+            return Ok(await _productService.CreateProduct(product));
+        }
+
+    }
+}
