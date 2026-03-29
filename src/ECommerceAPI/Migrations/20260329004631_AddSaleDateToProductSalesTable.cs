949db2bd930f9d50e86b496b5b57515617394850
clint@debian <clintbamp@gmail.com>
2026-03-29 00:52:18 +0000

    build: modified ProductSales table to include SaleDate property

---
+++ src/ECommerceAPI/Migrations/20260329004631_AddSaleDateToProductSalesTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSaleDateToProductSalesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalCost",
+                table: "ProductSales",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SaleDate",
+                table: "ProductSales",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SaleDate",
+                table: "ProductSales");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalCost",
+                table: "ProductSales",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
